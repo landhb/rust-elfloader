b40f18fc9f396c4442382a4626b97be3e6c56870
landhb <landhb@users.noreply.github.com>
2022-03-05 12:26:42 -0500

    Add tilegx relocation types

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -3,11 +3,15 @@ use crate::{ElfLoaderErr, Machine};
 pub mod x86;
 pub mod x86_64;
 
+pub mod tilegx;
+pub use tilegx as TileGX;
+
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
 pub enum RelocationType {
     x86(x86::RelocationTypes),
     x86_64(x86_64::RelocationTypes),
+    TileGX(TileGX::RelocationTypes),
 }
 
 impl RelocationType {
@@ -16,6 +20,7 @@ impl RelocationType {
         let typ = match machine {
             Machine::X86 => RelocationType::x86(x86::RelocationTypes::from(type_num)),
             Machine::X86_64 => RelocationType::x86_64(x86_64::RelocationTypes::from(type_num)),
+            Machine::Other(191) => RelocationType::TileGX(TileGX::RelocationTypes::from(type_num)),
             _ => return Err(ElfLoaderErr::UnsupportedArchitecture),
         };
         Ok(typ)
